e2fb57c2334e17d1c44345b6624c16b8f481cd0f
Damir Ferizovic <amtrix92@gmail.com>
2020-12-02 00:00:05 -0800

    Tiny update

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cp $MYDIR//build//webdash $MYWORLD/app-persistent/bin/
 echo "Successfully installed newest webdash."
